e8e2fc1157c4c4fd1a13ebf86c5facd000d674d5
Ryoko Oka <ryooo.kooo.22@gmail.com>
2021-03-10 16:46:12 -0500

    checking sql connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 5000; // should different to app.js PORT(client side)
 
 // this catches every route request - every time you change your location bar, this function reacts
 // and intercepts the roue request
